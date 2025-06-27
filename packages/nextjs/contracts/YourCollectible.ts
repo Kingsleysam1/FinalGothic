01b400c9f03992c2b3d6e7bee5eaa80b952d127d
Kingsleysam <Semeka90@gmail.com>
2025-06-27 15:56:10 +0100

    fix: use Abi instead of ContractAbi

--- packages/nextjs/contracts/YourCollectible.ts
+++ packages/nextjs/contracts/YourCollectible.ts
@@ -1,7 +1,7 @@
-import { ContractAbi } from "viem";
+import { Abi } from "viem";
 import YourCollectibleAbi from "@/constants/YourCollectible.json";
 
 export const yourCollectible = {
   address: "0x375c30dd53A6e34D516b69edb66E59A4A5e25921", // your deployed address
-  abi: YourCollectibleAbi.abi as ContractAbi,
+  abi: YourCollectibleAbi.abi as Abi,
 } as const;
