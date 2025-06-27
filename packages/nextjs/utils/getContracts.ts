5f30bed38bf75292c07572e985e632996e5f95fa
Kingsleysam <Semeka90@gmail.com>
2025-06-27 16:17:02 +0100

    fix: correct Provider import for ESM build of ethers

--- packages/nextjs/utils/getContracts.ts
+++ packages/nextjs/utils/getContracts.ts
@@ -1,8 +1,6 @@
-// packages/nextjs/utils/getContract.ts
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "@/constants/contract";
 import { ethers } from "ethers";
-import type { Provider } from "@ethersproject/providers";
 
-export const getContract = (providerOrSigner: ethers.Signer | Provider) => {
-  return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, providerOrSigner);
+export const getContract = (runner: ethers.ContractRunner) => {
+  return new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, runner);
 };
